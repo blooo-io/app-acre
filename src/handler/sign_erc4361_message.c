c0977614ffccbe2edfdccfe4ba97b8b91cb831d0
Z4karia <zakaria@blooo.io>
2024-10-10 17:52:08 +0200

    feat: implement parsing for all erc4361 components

--- src/handler/sign_erc4361_message.c
+++ src/handler/sign_erc4361_message.c
@@ -14,57 +14,34 @@
 
 #include "handlers.h"
 
-#define MESSAGE_CHUNK_SIZE 64
-#define MAX_DOMAIN_LENGTH  64
-#define MAX_URI_LENGTH     64
+#define MESSAGE_CHUNK_SIZE  64
+#define MAX_DOMAIN_LENGTH   64
+#define MAX_URI_LENGTH      64
+#define MAX_VERSION_LENGTH  5
+#define MAX_NONCE_LENGTH    32
+#define MAX_DATETIME_LENGTH 32
 
 // static unsigned char const BSM_SIGN_MAGIC[] = {'\x18', 'B', 'i', 't', 'c', 'o', 'i', 'n', ' ',
 //                                                'S',    'i', 'g', 'n', 'e', 'd', ' ', 'M', 'e',
 //                                                's',    's', 'a', 'g', 'e', ':', '\n'};
 
-static size_t parse_domain(const uint8_t *buffer, size_t buffer_len, char *domain) {
-    size_t domain_length = 0;
-    while (domain_length < buffer_len && domain_length < MAX_DOMAIN_LENGTH - 1) {
-        if (buffer[domain_length] == ' ' || buffer[domain_length] == '\n') {
+static size_t parse_field(const uint8_t *buffer,
+                          size_t buffer_len,
+                          char *output,
+                          size_t max_length) {
+    size_t field_length = 0;
+    while (field_length < buffer_len && field_length < max_length - 1) {
+        if (buffer[field_length] == '\n' || buffer[field_length] == '\0' ||
+            buffer[field_length] == ' ') {
             break;
         }
-        domain_length++;
+        field_length++;
     }
 
-    memcpy(domain, buffer, domain_length);
-    domain[domain_length] = '\0';
+    memcpy(output, buffer, field_length);
+    output[field_length] = '\0';
 
-    return domain_length;
-}
-
-static size_t parse_address(const uint8_t *buffer, size_t buffer_len, char *address) {
-    size_t address_length = 0;
-    while (address_length < buffer_len && address_length < MAX_ADDRESS_LENGTH_STR - 1) {
-        if (buffer[address_length] == ' ' || buffer[address_length] == '\n') {
-            break;
-        }
-        address_length++;
-    }
-
-    memcpy(address, buffer, address_length);
-    address[address_length] = '\0';
-
-    return address_length;
-}
-
-static size_t parse_uri(const uint8_t *buffer, size_t buffer_len, char *uri) {
-    size_t uri_length = 0;
-    while (uri_length < buffer_len && uri_length < MAX_URI_LENGTH - 1) {
-        if (buffer[uri_length] == '\n' || buffer[uri_length] == '\0' || buffer[uri_length] == ' ') {
-            break;
-        }
-        uri_length++;
-    }
-
-    memcpy(uri, buffer, uri_length);
-    uri[uri_length] = '\0';
-
-    return uri_length;
+    return field_length;
 }
 
 static bool has_newline(const char *buffer, size_t length) {
@@ -101,7 +78,11 @@ void handler_sign_erc4361_message(dispatcher_context_t *dc, uint8_t protocol_ver
 
     char domain[MAX_DOMAIN_LENGTH] = {0};
     char address[MAX_ADDRESS_LENGTH_STR] = {0};
-    char uri[MAX_URI_LENGTH] = {0};  // Add this line
+    char uri[MAX_URI_LENGTH] = {0};
+    char version[MAX_VERSION_LENGTH] = {0};
+    char nonce[MAX_NONCE_LENGTH] = {0};
+    char issued_at[MAX_DATETIME_LENGTH] = {0};
+    char expiration_time[MAX_DATETIME_LENGTH] = {0};
 
     char leftover[MESSAGE_CHUNK_SIZE] = {0};
     size_t leftover_len = 0;
@@ -109,7 +90,11 @@ void handler_sign_erc4361_message(dispatcher_context_t *dc, uint8_t protocol_ver
     size_t parsing_buffer_len = 0;
 
     bool parsing_done = false;
-    bool uri_found = false;  // Add this line
+    bool uri_found = false;
+    bool version_found = false;
+    bool nonce_found = false;
+    bool issued_at_found = false;
+    bool expiration_time_found = false;
 
     unsigned int chunk_index = 0;
     while ((chunk_index < n_chunks || total_bytes_read < message_length) && !parsing_done) {
@@ -139,28 +124,67 @@ void handler_sign_erc4361_message(dispatcher_context_t *dc, uint8_t protocol_ver
         leftover_len = 0;
 
         if (current_line == 0) {
-            size_t domain_length = parse_domain(parsing_buffer, parsing_buffer_len, domain);
-            total_bytes_read += domain_length - 1;  // -1 to skip the '\0' after the domain
+            size_t domain_length =
+                parse_field(parsing_buffer, parsing_buffer_len, domain, MAX_DOMAIN_LENGTH);
+            total_bytes_read += domain_length;
             PRINTF("Domain: %s\n", domain);
         }
 
         if (current_line == 1) {
-            size_t address_length = parse_address(parsing_buffer, parsing_buffer_len, address);
-            total_bytes_read += address_length - 1;  // -1 to skip the '\0' after the address
+            size_t address_length =
+                parse_field(parsing_buffer, parsing_buffer_len, address, MAX_ADDRESS_LENGTH_STR);
+            total_bytes_read += address_length;
             PRINTF("Address: %s\n", address);
         }
 
-        // Line 2 and 3 are empty
+        // Lines 2 and 3 are empty
         if (current_line >= 4) {
-            // Compare the start of the line with "URI"
             if (!uri_found && parsing_buffer_len >= 5 && memcmp(parsing_buffer, "URI: ", 5) == 0) {
-                size_t uri_length = parse_uri(parsing_buffer + 5, parsing_buffer_len - 5, uri);
+                size_t uri_length =
+                    parse_field(parsing_buffer + 5, parsing_buffer_len - 5, uri, MAX_URI_LENGTH);
                 total_bytes_read += uri_length + 5;  // +5 for "URI: "
                 uri_found = true;
                 PRINTF("URI: %s\n", uri);
+            } else if (!version_found && parsing_buffer_len >= 9 &&
+                       memcmp(parsing_buffer, "Version: ", 9) == 0) {
+                size_t version_length = parse_field(parsing_buffer + 9,
+                                                    parsing_buffer_len - 9,
+                                                    version,
+                                                    MAX_VERSION_LENGTH);
+                total_bytes_read += version_length + 9;  // +9 for "Version: "
+                version_found = true;
+                PRINTF("Version: %s\n", version);
+            } else if (!nonce_found && parsing_buffer_len >= 7 &&
+                       memcmp(parsing_buffer, "Nonce: ", 7) == 0) {
+                size_t nonce_length = parse_field(parsing_buffer + 7,
+                                                  parsing_buffer_len - 7,
+                                                  nonce,
+                                                  MAX_NONCE_LENGTH);
+                total_bytes_read += nonce_length + 7;  // +7 for "Nonce: "
+                nonce_found = true;
+                PRINTF("Nonce: %s\n", nonce);
+            } else if (!issued_at_found && parsing_buffer_len >= 11 &&
+                       memcmp(parsing_buffer, "Issued At: ", 11) == 0) {
+                size_t issued_at_length = parse_field(parsing_buffer + 11,
+                                                      parsing_buffer_len - 11,
+                                                      issued_at,
+                                                      MAX_DATETIME_LENGTH);
+                total_bytes_read += issued_at_length + 11;  // +11 for "Issued At: "
+                issued_at_found = true;
+                PRINTF("Issued At: %s\n", issued_at);
+            } else if (!expiration_time_found && parsing_buffer_len >= 18 &&
+                       memcmp(parsing_buffer, "Expiration Time: ", 17) == 0) {
+                size_t expiration_time_length = parse_field(parsing_buffer + 17,
+                                                            parsing_buffer_len - 17,
+                                                            expiration_time,
+                                                            MAX_DATETIME_LENGTH);
+                total_bytes_read += expiration_time_length + 17;  // +17 for "Expiration Time: "
+                expiration_time_found = true;
+                PRINTF("Expiration Time: %s\n", expiration_time);
             }
         }
 
+        // Iterate on the rest of the line
         for (size_t j = total_bytes_read - line_start; j < parsing_buffer_len; j++) {
             total_bytes_read++;
             PRINTF("Total bytes read: %llu\n", total_bytes_read);
